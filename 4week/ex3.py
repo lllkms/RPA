e79ff1f7db8b2e6d3e013b1a25f5067abcfcb988
unknown <lkm7632@naver.com>
2024-09-26 12:33:43 +0900

    4주차 실습파일

---
+++ 4week/ex3.py
@@ -0,0 +1,16 @@
+def sum_int(a,b):
+    return a+b
+
+def nud_int(a,b):
+    return a*b
+
+
+num1 = 10
+num2 = 20
+
+result = sum_int(num1, num2)
+print(f'{num1} + {num2} = {result}')
+
+result = nud_int(num1, num2)
+print(f'{num1} * {num2} = {result}')
+
---
+++ 4week/ex4.py
@@ -0,0 +1,14 @@
+def sumfunc(n):
+    return sum(range(1, n+1))
+
+try:
+    number = int(input("1이상의 정수를 입력 하세요 : "))
+    if number >= 1:
+        result = sumfunc(number)
+        print(f"1~{number}까지 정수의 합은 {number} 입니다.")
+        if result == 210:
+            print("합은 210 입니다.20 ")
+    else:
+        print("1이상의 정수를 입력해주세요.")
+except ValueError:
+    print("유효한 정수를 입력하세요.")
