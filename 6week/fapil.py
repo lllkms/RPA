419fb282fd45897ea4911bf64f4263f71540f555
unknown <lkm7632@naver.com>
2024-10-24 10:42:11 +0900

    7주차 실습파일

--- 6week/fapil.py
+++ 6week/fapil.py
@@ -9,13 +9,14 @@ def read_root():
 def read_item(item_id: int, name: str = None, age: int = 0):
     return {"item_id" : item_id, "name" : name, "age" : age}
 
-from fastapi.staticfiles import StaticFiles
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 from  fastapi import Form
 @app.post("/user")
 async def read_user_form(name: str = Form(...), studentcode: str = Form(...), major: str = Form(...)):
     return {"msg": f"{major} {name}님 ({studentcode}) 반갑습니다."}
+
+from fastapi.staticfiles import StaticFiles
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == '__main__':
     import uvicorn
